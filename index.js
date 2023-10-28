874090de2bc1d97978d628b8463d6cf70722f947
Pravash Thakuri <thakuripravashz@gmail.com>
2023-10-28 10:36:16 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const app = require('./src/config/express.config')
+const server = http.createServer(app);
+
+server.listen('3005', 'localhost', (err)=>{
+    if(!err){
+        console.log("Server Is Running Sir ^_^");
+    }
+})
